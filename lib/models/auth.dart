887c543ee75077bc4a87561158edd4fac29b69fc
Aguiar <beca.cout@gmail.com>
2023-02-28 23:43:40 -0300

    auth exceptions

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,19 +1,39 @@
 import 'dart:convert';
 
+import 'package:desingprojeto/exceptions/auth_exceptions.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class Auth with ChangeNotifier {
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA2iGUM3byFkKjBIwIVRulLdTMND3fz7LE';
+import '../utils/constants.dart';
 
-  Future<void> singUp(String email, String password) async {
-    var response = await http.post(Uri.parse(_url),
-        body: jsonEncode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-    print(response.body);
+class Auth with ChangeNotifier {
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=${Constants.webApiKey}';
+    final response = await http.post(
+      Uri.parse(url),
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      }),
+    );
+
+    final body = jsonDecode(response.body);
+
+    if (body['error'] != null) {
+      throw AuthException(body['error']['message']);
+    }
+
+    print(body);
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
